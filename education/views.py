2ce340439d6d6c6c5a3385bfb9daace016623548
amirhosein-abbasi <amirhosein.ai92@gmail.com>
2022-07-09 12:27:09 +0300

    courses

--- education/views.py
+++ education/views.py
@@ -1,6 +1,6 @@
 from multiprocessing import context
-from django.shortcuts import render
-from .models import CourseModel
+from django.shortcuts import render, get_object_or_404
+from .models import CourseModel, LessonModel
 
 
 def sniper_education(request):
@@ -11,8 +11,25 @@ def introduction(request):
     return render(request, 'education/introduction.html')
 
 def all_courses(request):
-    courses = CourseModel.objects.all()
+    courses = CourseModel.objects.all().filter(chapter=1)
+    lessons = LessonModel.objects.all().filter(chapter=1)
     context = {
-        'courses': courses
+        'courses': courses,
+        'lessons' : lessons,
     }
-    return render(request, 'education/all_courses.html', context)
+    return render(request, 'education/all_courses_chapter1.html', context)
+
+
+def course_detail(request, course_id):
+    main_course = get_object_or_404(CourseModel, id=course_id)
+    context = {
+        'main_course': main_course
+    }
+    return render(request, 'education/.html', context)
+
+
+def lesson_detail(request, lesson_id):
+    main_lesson = get_object_or_404(LessonModel, id=lesson_id)
+    context = {
+
+    }
