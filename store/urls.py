f61f5162f86a444932d2932eee7231e15a20c9ad
amirhosein-abbasi <amirhosein.ai92@gmail.com>
2022-08-09 20:56:50 +0300

    mailbox for newsletter

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('store_homepage', store_homepage, name='store_homepage'),
+    path('store_newsletter', store_newsletter, name='store_newsletter'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from pages.models import NewsletterModel
 
 def store_homepage(request):
     return render(request, 'store/store.html')
+
+
+
+def store_newsletter(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        subscriber = NewsletterModel(user= request.user,email=email)
+        subscriber.save()
+    return redirect('index')
