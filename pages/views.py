be2433b3451c4d8afb0cb67ce605d220190f89e9
amirhosein-abbasi <amirhosein.ai92@gmail.com>
2022-08-15 20:44:35 +0300

    events

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,53 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from .models import NewsletterModel, ContactModel
+from event.models import Event
+from datetime import date, datetime
 
 def index(request):
-    return render(request, 'pages/index.html')
+    # today = date.today()
+    today = datetime.now().strftime ("%d%-H%M%-S")
+    b = today
+    today  = str(today)
+    today_day = today[0:2]
+    tokyo_session = Event.objects.all().filter(title='Tokyo Session', status='Upcoming')
+    for ts in tokyo_session:
+        when_ts_d =ts.when.strftime ("%d")
+        when_ts_hours =ts.when.strftime ("%-H")
+        when_ts_min =ts.when.strftime ("%M")
+        when_ts_sec =ts.when.strftime ("%-S")
+    
+    
+
+    london_session = Event.objects.all().filter(title='London Session', status='Upcoming')
+    for ls in london_session:
+        when_ls_d =ls.when.strftime ("%d")
+        when_ls_hours =ls.when.strftime ("%-H")
+        when_ls_min =ls.when.strftime ("%M")
+        when_ls_sec =ls.when.strftime ("%-S")
+    
+    
+    ny_session = Event.objects.all().filter(title='NY Session', status='Upcoming')
+    for ny in ny_session:
+        when_ny_d =ny.when.strftime ("%d")
+        when_ny_hours =ny.when.strftime ("%-H")
+        when_ny_min =ny.when.strftime ("%M")
+        when_ny_sec =ny.when.strftime ("%-S")
+    
+    ts_dict = {'when_ts_d':when_ts_d, 'when_ts_hours':when_ts_hours, 'when_ts_min':when_ts_min, 'when_ts_sec':when_ts_sec}
+    ls_dict = {'when_ls_d':when_ls_d, 'when_ls_hours':when_ls_hours, 'when_ls_min':when_ls_min, 'when_ls_sec':when_ls_sec}
+    ny_dict = {'when_ny_d':when_ny_d, 'when_ny_hours':when_ny_hours, 'when_ny_min':when_ny_min, 'when_ny_sec':when_ny_sec}
+    
+    context = {
+        'tokyo_session': tokyo_session,
+        'london_session':london_session,
+        'ny_session':ny_session,
+        'ts_dict':ts_dict,
+        'ls_dict': ls_dict,
+        'ny_dict': ny_dict,
+    }
+
+    return render(request, 'pages/index.html', context)
 
 
 def contact_us(request):
@@ -25,3 +70,7 @@ def send_message_to_admin(request):
         contact = ContactModel(full_name=name_, number=number, email=email, message=msg)
         contact.save()
     return redirect('index')
+
+
+    
+   
